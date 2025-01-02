d42186221ef8634c53131bfd5f995d77287b9a63
Sazzad Hissain Khan <hissain.khan@gmail.com>
2025-01-02 22:54:40 +0600

    Added ollama support

--- app_patent/app.py
+++ app_patent/app.py
@@ -1,80 +1,106 @@
 import os
 import time
+import logging
 import pandas as pd
 import streamlit as st
 from prompts import prompt_keywords, prompt_query
 from duckduckgo_search import DDGS
+from googlesearch import search
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
-
 from dotenv import load_dotenv
+from langchain_ollama import OllamaLLM
+
 load_dotenv()
 
-# Define LLM
-llm = ChatGoogleGenerativeAI(
-    model="gemini-pro",
-    google_api_key=os.getenv('GEMINI_API_KEY'),
-    temperature=0.1
-)
+logging.basicConfig(level=logging.INFO)
+
+def log_message(message):
+    logging.info(message)
+    st.write(message)
+
+def get_model():
+
+    if llm_engine in "Gemini":
+        return ChatGoogleGenerativeAI(
+            model="gemini-pro",
+            google_api_key=os.getenv('GEMINI_API_KEY'),
+            temperature=0.1)
+    else:
+        return OllamaLLM(model=llm_model_name)
+
+llm = OllamaLLM(model="llama3")
 
 def generate_keywords(topic):
     prompt = PromptTemplate.from_template(prompt_keywords)
-    chain = prompt | llm
+    chain = prompt | get_model()
     result = chain.invoke({"topic": topic})
-    keywords = [keyword.strip() for keyword in result.content.split(',')]
-    return keywords
+    return [keyword.strip() for keyword in result.content.split(',')]
 
 def generate_queries(topic, keywords):
     prompt = PromptTemplate.from_template(prompt_query)
-    chain = prompt | llm
+    chain = prompt | get_model()
     result = chain.invoke({"topic": topic, "keywords": keywords})
-    queries = [q.strip() for q in result.content.split(',')]
-    return queries
+    return [q.strip() for q in result.content.split(',')]
 
-def search_duckduckgo(query):
+def search_duckduckgo(query, max_results=10):
     with DDGS(verify=False) as ddgs:
-        results = ddgs.text(query, max_results=10)
-    return results
+        return [
+            {
+                'Title': result.get('title'),
+                'URL': result.get('href'),
+                'Description': result.get('body')
+            }
+            for result in ddgs.text(query, max_results=max_results)
+        ]
 
-def parse_results_to_dataframe(results):
-    data = [
+def search_google(query, max_results=10):
+    return [
         {
-            'Title': result.get('title'),
-            'URL': result.get('href'),
-            'Description': result.get('body')
+            'Title': result.title,
+            'URL': result.url,
+            'Description': result.description
         }
-        for result in results
+        for result in search(query, num_results=max_results, advanced=True)
     ]
-    df = pd.DataFrame(data)
-    df_filtered = df[df['URL'].str.startswith('https://patents.google.com/patent/')]
-    return df_filtered
 
-def search_prior_art(queries):
+def search_prior_art(queries, search_engine, k):
     full_result = []
     for query in queries:
-        full_query = f"site:patents.google.com {query}"
-        results = search_duckduckgo(full_query)
-        full_result.extend(results)
+        log_message(f"Processing Query: \n{query}")
+        if search_engine in ('duckduckgo', 'both'):
+            full_query = f"site:patents.google.com {query}"
+            results = search_duckduckgo(full_query, k)
+            full_result.extend(results)
+            log_message(f"{len(results)} results found for DDGS")
+        if search_engine in ('google', 'both'):
+            full_query = f"https://patents.google.com?q={query}"
+            results = search_google(full_query, k)
+            full_result.extend(results)
+            log_message(f"{len(results)} results found for GoogleSearch")
         time.sleep(2)
-    return parse_results_to_dataframe(full_result)
 
-# Streamlit app
+    log_message(f"{len(full_result)} results found in total")
+    return pd.DataFrame(full_result).query("URL.str.startswith('https://patents.google.com/patent/')", engine='python')
+
 st.title("Patent Search and Analysis Tool")
 st.write("Use this app to search for prior art related to your idea using DuckDuckGo and Google Patents.")
 
-# User input
 topic = st.text_area("Enter a description of your idea or topic:", "A new method for efficient solar energy conversion using nanotechnology.")
 
-# Sidebar configuration
 st.sidebar.header("Settings")
 top_k_results = st.sidebar.number_input("Number of Results per Query", min_value=1, max_value=20, value=10, step=1)
-api_key_input = st.sidebar.text_input("Google Gemini API Key", type="password")
+search_engine = st.sidebar.radio("Select Search Engine", options=['google', 'duckduckgo', 'both'], index=0)
+llm_engine = st.sidebar.radio("Select LLM Type", options=['Gemini', 'Local Ollama'], index=0)
 
-# Update environment variable if the API key changes
+if llm_engine == 'Gemini':
+    api_key_input = st.sidebar.text_input("Google Gemini API Key", type="password")
+elif llm_engine == 'Local Ollama':
+    llm_model_name = st.sidebar.text_input("Ollama Model Name", value="llama3.2:latest")
+
 if api_key_input and api_key_input != os.getenv('GEMINI_API_KEY'):
     os.environ['GEMINI_API_KEY'] = api_key_input
 
-# Search functionality
 if st.button("Search for Patents"):
     if not topic.strip():
         st.error("Please enter a valid topic description.")
@@ -90,16 +116,14 @@ if st.button("Search for Patents"):
             st.success("Queries generated successfully!")
             st.write(queries)
 
-            st.info("Searching for prior art...")
-            prior_arts = search_prior_art(queries)
+            st.info(f"Searching for prior art... top: {top_k_results}")
+            prior_arts = search_prior_art(queries, search_engine, top_k_results)
             st.success("Search completed!")
 
             if not prior_arts.empty:
                 st.subheader("Search Results")
                 for _, row in prior_arts.iterrows():
-                    st.write(
-                        f"### [{row['Title']}]({row['URL']})\n{row['Description']}"
-                    )
+                    st.write(f"### [{row['Title']}]({row['URL']})\n{row['Description']}")
             else:
                 st.warning("No results found for the given queries.")
 
